a9809c5ac31945f1f5a12f0263f4d6ca91ccedf5
Douglas Hugo <douglas.hugo@live.com>
2023-09-22 15:35:23 -0300

    desafio-arraylist

---
+++ desafio-arraylist/src/entities/Employee.java
@@ -0,0 +1,47 @@
+package entities;
+
+public class Employee {
+
+    public int id;
+    public String name;
+    public double salary;
+
+    public Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
